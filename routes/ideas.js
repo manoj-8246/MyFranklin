82d39e261c109ea774283ab43c0c777359b5329c
manoj-8246 <58218637+manoj-8246@users.noreply.github.com>
2021-03-08 12:34:01 +0530

    Update ideas.js

--- routes/ideas.js
+++ routes/ideas.js
@@ -154,6 +154,31 @@ router.post('/', function (req, res, next) {
 
 /*** buzzword handler function ***/
 function buzzWordHandler(req, res, next) {	
+	
+	try{
+		const result = app.client.chat.postMessage({
+		token: process.env.TOKEN,
+		channel: process.env.ChannelId,
+		text: "*Franklin - (How Franklin Works) Diagram*",
+		attachments:'[{"blocks":[{"type":"section","text":{"type":"mrkdwn","text":"Franklin is a Slack chatbot created by FFN."}},{"type":"actions","elements":[{"type":"button","text":{"type":"plain_text","text":"View Dataflow Diagram"},"url":"https://www.lucidchart.com/documents/view/a6565a46-8e2e-4516-98dd-d77b1e9f47af","style":"primary"}]}]}]',
+
+		});
+			console.log(result);
+		
+		return res.json({
+					fulfillmentText: result,					
+					source: 'BuzzWord'
+				}); 
+		
+		
+    }catch (error) {
+    return res.json({
+	fulfillmentText: 'Could not get results at this time',
+	source: 'howFranklinWorksHandler'
+	})
+  } 
+	
+	/*
 	https.get(
 		'https://corporatebs-generator.sameerkumar.website/',
 		responseFromAPI => {
@@ -193,6 +218,7 @@ function buzzWordHandler(req, res, next) {
 			})
 		}
 	)
+	*/
 		
 }
    
