8d0bbc5b2e18e00cfe24d0ef2509bf0866113b35
manoj-8246 <58218637+manoj-8246@users.noreply.github.com>
2021-03-09 10:19:11 +0530

    i have added dynamic channel id

--- routes/ideas.js
+++ routes/ideas.js
@@ -23,6 +23,7 @@ router.post('/', function (req, res, next) {
  var intentName = req.body.queryResult.intent.displayName;
     console.log(intentName);
      console.log('hello!');	
+ var ChannelID =req.body.originalDetectIntentRequest.payload.data.event.channel; 	
     try {
         switch (intentName) {	
 	     case "JIRA-NewIdea":                
@@ -161,7 +162,7 @@ function buzzWordHandler(req, res, next) {
 	try{
 		const result = app.client.chat.postMessage({
 		token: process.env.TOKEN,
-		channel:'D01ERH0GTBQ',
+		channel:ChannelID,
 		//channel:process.env.ChannelId,
 		text: "*Franklin - (How Franklin Works) Diagram*",
 		attachments:'[{"blocks":[{"type":"section","text":{"type":"mrkdwn","text":"Franklin is a Slack chatbot created by FFN."}},{"type":"actions","elements":[{"type":"button","text":{"type":"plain_text","text":"View Dataflow Diagram"},"url":"https://www.lucidchart.com/documents/view/a6565a46-8e2e-4516-98dd-d77b1e9f47af","style":"primary"}]}]}]',
