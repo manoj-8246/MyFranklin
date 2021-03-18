a79f35727fc62b4e7d4a45aa5223315e7991de6d
manoj-8246 <58218637+manoj-8246@users.noreply.github.com>
2021-03-18 12:14:42 +0530

    Update ideas.js

--- routes/ideas.js
+++ routes/ideas.js
@@ -1304,8 +1304,8 @@ function listAllFFNDictionaryHandler(req, res, next) {
 	token: process.env.TOKEN,
 	channel: req.body.originalDetectIntentRequest.payload.data.event.channel,
 	text: "Required income to net a specific income.",
-	//attachments:'[{"color": "#3AA3E3","blocks":"' + myblocks + '"}]',
-	attachments:'[{"color": "#3AA3E3","blocks":"' + Originalblocks + '"}]',
+	attachments:'[{"color": "#3AA3E3","blocks":{"type": "section","text": {"type": "mrkdwn","text": "hello"}}}]',
+	//attachments:'[{"color": "#3AA3E3","blocks":"' + Originalblocks + '"}]',
     });
 	console.log(result);
     }catch (error) {
